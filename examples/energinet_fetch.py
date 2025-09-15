9dcf3959277705e6b4c0effcbc74155440c095de
qmlai <quantitativeml@gmail.com>
2025-09-15 14:39:03 +0200

    print df

--- examples/energinet_fetch.py
+++ examples/energinet_fetch.py
@@ -11,4 +11,6 @@ if __name__ == "__main__":
     end     = pd.Timestamp(2025, 8, 30, tz="UTC").date().strftime("%Y-%m-%d")
     
     fetcher.fetch_all_curves(market=markets, start=start, end=end)
-    fetcher.load_curves(base_dir=ENERGINET_BASE_DATA_DIR, market="DK_1")
+    df = fetcher.load_curves(base_dir=ENERGINET_BASE_DATA_DIR, market="DK1")
+    print(df.head())
+
